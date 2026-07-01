df7a8dc45217c5dcd4bbea12bb61afa330960ce7
Temirlan Zhanbolat <tomikastana2018@gmail.com>
2026-07-02 00:04:11 +0500

    Add Master Password auth via SHA-256 and restore plain text storage display

--- password-manager.py
+++ password-manager.py
@@ -1,3 +1,9 @@
+import json
+import hashlib
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
 class PasswordEntry:
     def __init__(self, service, username, password):
         self.service = service
@@ -23,11 +29,40 @@ class PasswordManager:
         for entry in self.entries:
             entry.show_details()
 
+    def save_to_file(self):
+        data_to_save = []
+        for entry in self.entries:
+            entry_dict = {
+                "service": entry.service,
+                "username": entry.username,
+                "password": entry.password
+            }
+            data_to_save.append(entry_dict)
+
+        with open("passwords.json", "w") as file:
+            json.dump(data_to_save, file, indent=4)
+
+    def load_from_file(self):
+        try:
+            with open("passwords.json", "r") as file:
+                data = json.load(file)
+            for item in data:
+                entry = PasswordEntry(item["service"], item["username"], item["password"])
+                self.entries.append(entry)
+        except FileNotFoundError:
+            return
+
+
 manager = PasswordManager()
-password1 = PasswordEntry("Google", "Tomik123", "917302")
-password2 = PasswordEntry("Telegram", "Tomik123", "917302")
-manager.add_entry(password1)
-manager.add_entry(password2)
+manager.load_from_file()
+
+master_input = input("Enter Master Password to unlock: ")
+
+if hash_password(master_input) == "8c5b79cdab71169ff689e798250f3157dbf42c81c6c87c62c5d46c2c62febe65":
+    print("Access Granted!")
+else:
+    print("Access Denied!")
+    exit()
 
 while True:
     print("Welcome to Password Manager!")
@@ -41,6 +76,7 @@ while True:
         username = input("Enter username: ")
         password = input("Enter password: ")
         manager.add_entry(PasswordEntry(service, username, password))
+        manager.save_to_file()
 
     elif choice == 2:
         manager.show_all_entries()
@@ -48,6 +84,7 @@ while True:
     elif choice == 3:
         print("Thank you for using Password Manager!")
         break
+
 
 
 
